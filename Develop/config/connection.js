4032efd789cb90e6cb04c43a23d058e81eaaa1d9
Mitsushiro Hirano <mitsushiro1@gmail.com>
2023-02-24 21:20:40 -0500

    added some stuff

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -4,12 +4,31 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
+  : new Sequelize('ecommerce_db', 'root', 'root', {
+      host: 'localhost',  
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
       },
+    
     });
 
 module.exports = sequelize;
+
+
+// const Sequelize = require('sequelize');
+// require('dotenv').config();
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306,
+//   }
+// );
+
+// module.exports = sequelize;
+
--- Develop/models/index.js
+++ Develop/models/index.js
@@ -6,11 +6,26 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Product.belongsTo(Category, {
+  foreignKey: 'ecommerce_id',
+});
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'ecommerce_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
